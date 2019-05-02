cb07c758f80a1c04dacf2ece236cc94f10f0b6b2
Ali Bagheri <a6bagher@uwaterloo.ca>
2019-05-02 00:00:01 -0400

    Update comparator.cpp

--- comparator.cpp
+++ comparator.cpp
@@ -27,3 +27,31 @@ public:
         }
     }
 };
+
+
+
+
+bool compare(Player a, Player b) {
+    if(Checker::comparator(a,b) == -1)
+        return false;
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player player;
+        player.name = name, player.score = score;
+        players.push_back(player);
+    }
+    sort(players.begin(), players.end(), compare);
+    for(int i = 0; i < players.size(); i++) {
+        cout << players[i].name << " " << players[i].score << endl;
+    }
+    return 0;
+}
